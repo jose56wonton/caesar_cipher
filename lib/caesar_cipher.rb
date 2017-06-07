bef8802ee3bce369d626748a091a130c599f4a63
joshua wootonn <joshuawootonn@gmail.com>
2017-06-06 23:23:01 -0500

    Reorganized with correct file orietation and added some basic specs

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -11,15 +11,6 @@ def caesar_cipher( words , shift )
     end
     encoded_msg += char_ciphered
   end
-  puts "Your msg was: #{encoded_msg}"
+  encoded_msg
 
 end
-
-caesar_cipher("a", 1)
-caesar_cipher("b", 1)
-caesar_cipher("c", 1)
-
-
-caesar_cipher("x", 1)
-caesar_cipher("y", 1)
-caesar_cipher("z", 1)
